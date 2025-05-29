00649e4c9602ea165bcba8ef85d2321b5fad56ea
ivanilyich <natsios.chr@gmail.com>
2025-05-29 16:15:29 +0300

    fix: update CSP configuration for anime.js animation - Add unsafe-eval, disable in dev, comment sitemap, update font-src

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,54 +1,88 @@
 const config = require('./src/config');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   pathPrefix: '/portfolio',
   siteMetadata: {
     title: 'Ivan Ilyich',
-    description: 'Personal portfolio and blog',
-    siteUrl: 'https://ivan-ilyich.github.io/portfolio',
-    image: '/portfolio/og.png',
-    twitterUsername: '@bchiang7',
+    description: 'Software Engineer & Developer',
+    siteUrl: 'https://ivanilyich.github.io/portfolio',
+    image: '/og.png',
+    twitterUsername: '@ivanilyich',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-robots-txt`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'Ivan Ilyich Portfolio',
-        short_name: 'Portfolio',
-        start_url: '/portfolio/',
-        background_color: '#0a192f',
+        name: 'Ivan Ilyich',
+        short_name: 'Ivan Ilyich',
+        description: 'Software Engineer & Developer',
+        start_url: '/portfolio',
+        background_color: '#020c1b',
         theme_color: '#0a192f',
         display: 'minimal-ui',
-        icon: 'static/favicon-32x32.png',
-        include_favicon: true,
-        legacy: true,
-        theme_color_in_head: true,
-        cache_busting_mode: 'query',
-        crossOrigin: `use-credentials`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `static`,
-        path: `${__dirname}/static`,
+        icon: 'src/images/logo.png',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        trackingId: 'G-XXXXXXXXXX',
+        head: true,
+        anonymize: true,
       },
     },
+    // {
+    //   resolve: 'gatsby-plugin-sitemap',
+    //   options: {
+    //     query: `
+    //       {
+    //         site {
+    //           siteMetadata {
+    //             siteUrl
+    //           }
+    //         }
+    //         allSitePage {
+    //           edges {
+    //             node {
+    //               path
+    //             }
+    //           }
+    //         }
+    //       }
+    //     `,
+    //     serialize: ({ site, allSitePage }) =>
+    //       allSitePage.edges.map(edge => ({
+    //         url: `${site.siteMetadata.siteUrl}${edge.node.path}`,
+    //         changefreq: 'daily',
+    //         priority: 0.7,
+    //       })),
+    //   },
+    // },
+    {
+      resolve: 'gatsby-plugin-csp',
+      options: {
+        disableOnDev: true,
+        mergeScriptHashes: true,
+        mergeStyleHashes: true,
+        directives: {
+          'default-src': "'self'",
+          'script-src': "'self' 'unsafe-inline' 'unsafe-eval' blob:",
+          'style-src': "'self' 'unsafe-inline'",
+          'img-src': "'self' data: blob:",
+          'connect-src': "'self'",
+          'font-src': "'self' data:",
+          'object-src': "'none'",
+        },
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -57,25 +91,10 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/content/posts`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `projects`,
-        path: `${__dirname}/content/projects`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            // https://www.gatsbyjs.org/packages/gatsby-remark-external-links
             resolve: 'gatsby-remark-external-links',
             options: {
               target: '_blank',
@@ -83,58 +102,22 @@ module.exports = {
             },
           },
           {
-            // https://www.gatsbyjs.org/packages/gatsby-remark-images
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 700,
               linkImagesToOriginal: true,
               quality: 90,
-              tracedSVG: { color: config.colors.green },
+              tracedSVG: { color: '#64ffda' },
             },
           },
           {
-            // https://www.gatsbyjs.org/packages/gatsby-remark-code-titles/
-            resolve: 'gatsby-remark-code-titles',
-          }, // IMPORTANT: this must be ahead of other plugins that use code blocks
-          {
-            // https://www.gatsbyjs.org/packages/gatsby-remark-prismjs
-            resolve: `gatsby-remark-prismjs`,
+            resolve: 'gatsby-remark-prismjs',
             options: {
-              // Class prefix for <pre> tags containing syntax highlighting;
-              // defaults to 'language-' (e.g. <pre class="language-js">).
-              // If your site loads Prism into the browser at runtime,
-              // (e.g. for use with libraries like react-live),
-              // you may use this to prevent Prism from re-processing syntax.
-              // This is an uncommon use-case though;
-              // If you're unsure, it's best to use the default value.
               classPrefix: 'language-',
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
               inlineCodeMarker: null,
-              // This lets you set up language aliases.  For example,
-              // setting this to '{ sh: "bash" }' will let you use
-              // the language "sh" which will highlight using the
-              // bash highlighter.
               aliases: {},
-              // This toggles the display of line numbers globally alongside the code.
-              // To use it, add the following line in gatsby-browser.js
-              // right after importing the prism color scheme:
-              //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-              // Defaults to false.
-              // If you wish to only show line numbers on certain code blocks,
-              // leave false and use the {numberLines: true} syntax below
               showLineNumbers: false,
-              // If setting this to true, the parser won't handle and highlight inline
-              // code used in markdown i.e. single backtick code like `this`.
               noInlineHighlight: false,
-              // This adds a new language definition to Prism or extend an already
-              // existing language definition. More details on this option can be
-              // found under the header "Add new language definition or extend an
-              // existing language" below.
               languageExtensions: [
                 {
                   language: 'superscript',
@@ -149,22 +132,15 @@ module.exports = {
                   },
                 },
               ],
-              // Customize the prompt used in shell output
-              // Values below are default
               prompt: {
                 user: 'root',
                 host: 'localhost',
                 global: false,
               },
+              escapeEntities: {},
             },
           },
         ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-45666519-2',
       },
     },
   ],
